962d7c1295cc3459aeb9d029b732ef8f57ca4a18
Pedro Martinez <pedromartinez@MacBook-Pro-de-Pedro.local>
2019-10-16 12:19:54 -0500

    managing data with files

--- express_learn3/models/product.js
+++ express_learn3/models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+//const products = [];
+
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product{
     constructor(title){
@@ -6,10 +9,33 @@ module.exports = class Product{
     }
 
     save(){
-        products.push(this);
+        //products.push(this);
+        //usando files para guardar la data
+        
+        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+        fs.readFile(p,(err, fileContent)=>{
+            let products = [];
+            if(!err){
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err=>{
+                console.log(err);
+            });
+        });
     }
 
     static fetchAll(){
-        return  products;
+        //return  products;
+        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+
+        //recorda QUE ESTE METODO SE EJECUTA ASINCRONAMENTE!!!!
+        //por eso el que reciba el resultado de la lectura del archivo recibirá undefined!!
+        fs.readFile(p, (err, fileContent)=>{
+            if(err){
+                return [];
+            }
+            return JSON.parse(fileContent);
+        });
     }
 }
