6e94b7adb8f316142fad4e5f40bd6e97d2a972c0
Pedro Martinez <pedromartinez@MacBook-Pro-de-Pedro.local>
2019-10-21 11:45:20 -0500

    Add to cart implementation

--- nodejs_new_enhaced_maxi/controllers/shop.js
+++ nodejs_new_enhaced_maxi/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -9,6 +10,18 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.getProduct = (req,res,next) => {
+  //uso el mismo nombre del parametro que definí en el router!!
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      pageTitle: product.title,
+      product:product,
+      path: '/products'
+    });
+  });
+}
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -25,6 +38,17 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req,res,next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId,product.price);
+    //console.log('product.price', product.price);
+  });
+  
+  
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
--- nodejs_new_enhaced_maxi/routes/shop.js
+++ nodejs_new_enhaced_maxi/routes/shop.js
@@ -10,7 +10,17 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+//usamos el comodin dinamico dos puntos
+//ojo que a pesar de que estamos usando el metodo get() y este hace que las
+//rutas se comporten como "exact", el comodin hace que esa parte de la ruta 
+//ya NO sea exact, por lo tanto tener cuidado con el orden de las rutas para
+//que el comodin no impida que se llegue a otrasa rutas mas abajo, por ejemplo
+//si debajo de la ruta comodin pongo /products/delete , esta ruta nunca será alcanzada!!
+router.get('/products/:productId',shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
+
+router.post('/cart',shopController.postCart)
 
 router.get('/orders', shopController.getOrders);
 
