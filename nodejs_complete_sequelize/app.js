3b7e748461f5b15a12816982721504cd4f598345
Pedro Martinez <pedromartinez@MacBook-Pro-de-Pedro.local>
2019-11-01 11:39:10 -0500

    sequeliz - creating user and manyToMany cart item  models

--- nodejs_complete_sequelize/app.js
+++ nodejs_complete_sequelize/app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -33,18 +38,61 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//creo este MW para cargar el usuario dummy que creé al iniciar mi servidor
+//este usuario lo anexo al request
+//recordar que los MW solo se suscriben al event loop la primera vez que levanto mi server
+//por lo tanto este MW solo se ejecutará cuando entren los requests!!, lo cual garantiza
+//que el user esté disponible para cuando empiezen los requests
+app.use((req,res,next)=>{
+    User.findByPk(1)//recordar que todos los metodos de sequelize devuelven los valores dentro de una Promesa!!
+    .then(user=>{
+        req.user = user;
+        //ademas recordar que este user devuelto puede invocar a save(), destroy(), etc
+
+        next();//no olvidar el next, estamos dentro de un app.use()!!!!
+    })
+    .catch(err=>console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+
+//hacemos las relaciones entre tablas antes de sincronizar
+Product.belongsTo(User, {constraint:true, onDelete: 'CASCADE'});
+//esto equivaldria a lo de arriba : uno a muchos. no sería necesario ponerlo
+User.hasMany(Product);
+
+//relaciones para Cart
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
+
 // sincronizamos con la BD. Sequelize crea las tablas si no existieran aun
 // ademas te agrega un par de campos a tus tablas : createAt y updatedAt
 //sync devuelve una promesa
-sequelize.sync()
+sequelize.sync({force:true})//con force true en dev obligamos a limpiar siempre las tablas
+//sequelize.sync()
 .then(result=>{
     //console.log(result);
-    app.listen(3000);//recien inicio mi server cuando tenga conexion con la BD!
+    //quiero crear un usuario dummy al vuelo!
+    //primero verifico si ya existe
+    return User.findByPk(1);//lo retorno como promesa
+})
+.then(user=>{
+    if(!user){
+        return User.create({name:'Perico', email:'periquito@pinpin'});
+    }
+    return user; //CUALQUIER VALOR QUE RETORNE DESDE UN 'then' AUTOMATICAMENTE SERÅ ENVUELTO EN UNA PROMESA!!!
+})
+.then(user=>{
+    console.log(user);
+    app.listen(3000);//ejecuto mi servidor despues de haber creado mis tablas y mi usuario dummy
 })
 .catch(err=>{
     console.log(err);
