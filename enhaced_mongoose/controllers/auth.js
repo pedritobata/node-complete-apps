8cb04c38bdf1fb0c0797a35c0091eff4cdfe4f0e
Pedro Martinez <pedromartinez@MacBook-Pro-de-Pedro.local>
2019-11-07 14:09:06 -0500

    postLogin implemented - first part

--- enhaced_mongoose/controllers/auth.js
+++ enhaced_mongoose/controllers/auth.js
@@ -2,6 +2,20 @@
 exports.getLogin = (req,res,next) => {
     res.render('auth/login', {
         pageTitle: 'Login',
-        path: '/login'
+        path: '/login',
+        isAuthenticated: req.isLoggedIn
     });
+};
+
+exports.postLogin = (req,res,next) => {
+    //si cargamos un atributo en el request e inmediatamente llamamos
+    //a redirect, con esto ya se emitió un response, lo cual hace que el request anterior
+    //manque y por lo tanto se habrá perido mi atributo de login
+    req.isLoggedIn = true;
+
+    //la diferencia con el atributo user que cargamos en el request en app.js es que
+    //como esta dentro de un MW, este se ejecutará con CADA REQUEST que se genere, por eso
+    //siempre el user está disponible en cada controller
+
+    res.redirect('/');
 };
