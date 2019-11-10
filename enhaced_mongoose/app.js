8ea21c9598810a31611cbcf07d389bc35e7be1cc
Pedro Martinez <pedromartinez@MacBook-Pro-de-Pedro.local>
2019-11-10 12:31:56 -0500

    Using sessions first part

--- enhaced_mongoose/app.js
+++ enhaced_mongoose/app.js
@@ -7,7 +7,16 @@ const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const MONGODB_URI = 'mongodb+srv://pedro:R@tamacue1@cluster0-rw1t7.mongodb.net/shop?';
+
 const app = express();
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'//nombre a criterio
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,6 +27,18 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'my secret',
+  resave: false,//para que no se guarde la session con cada request
+  saveUninitialized: false//para que no se genere la cookie de IDsession en sesiones no iniciadas
+  //tambien se puede configurar las cookies
+  ,cookie : {
+    //aca podemos configurar el maxAge, expires, etc
+    //El servidor crea una cookie que será enviada al cliente para que este nos la
+    //envie en los subssiguientes requests
+  },
+  store: store
+}));
 
 app.use((req, res, next) => {
   User.findById('5dc43f609912b105223839db')
@@ -52,7 +73,7 @@ mongoose
         user.save();
       }
     });
-    console.log('Connected through Mongoose!!!');
+    console.log('Connected via Mongoose!!!');
     app.listen(3000);
   })
   .catch(err => {
--- enhaced_mongoose/controllers/auth.js
+++ enhaced_mongoose/controllers/auth.js
@@ -1,9 +1,27 @@
 
 exports.getLogin = (req,res,next) => {
+    console.log(req.get('Cookie'));
+    //ojo que el cliente nos va a enviar las cookies siempre por defecto!!
+    //es por eso que las podemos obtener del request que viene del cliente
+    //con la cookie ya podemos saber si el user estaba autenticado
+    //tener en cuenta que el valor true que llega de la cookie es solo UN STRING!!!
+    //hacemos una pequeña validacion para enviar a nuestra vista true COMO BOOLEANO!!
+
+    //Ojo que el cliente nos envia todas las cookies que tenga, incluso las que les han creado otros web sites!!
+    //asimismo, el cliente le enviará nuestras cookies a otros sites!!
+
+    //Acá hay un guardaza!!. el cliente puede manipular la cookie manualmente en su browser y
+    //ponerle siempre true. esto se debe solucionar y mejor , para operaciones sensibles
+    //usar SESSION
+    //  const loggedIn = req.get('Cookie').split('=')[1].trim() === 'true';
+
+    //con session
+    const loggedIn = req.session.loggedIn;
+    console.log(req.session);
     res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: loggedIn
     });
 };
 
@@ -11,11 +29,29 @@ exports.postLogin = (req,res,next) => {
     //si cargamos un atributo en el request e inmediatamente llamamos
     //a redirect, con esto ya se emitió un response, lo cual hace que el request anterior
     //manque y por lo tanto se habrá perido mi atributo de login
-    req.isLoggedIn = true;
-
     //la diferencia con el atributo user que cargamos en el request en app.js es que
     //como esta dentro de un MW, este se ejecutará con CADA REQUEST que se genere, por eso
     //siempre el user está disponible en cada controller
+    //req.isLoggedIn = true;
+
+    //Se podría usar una cookie (que al final no es segura para autenticacion)
+    //las cookies guardan info que el servidor envía al cliente y éste la guarda en el browser
+    //las cookies se pueden configurar pasando parametros separados por ";".
+    //se puede configurar: Expires=  ; Domain= (qué dominio aceptamos)  ;  
+    //Max-Age=   ; Secure  (la cookie se genera solo si el cliente usa https) ;
+    //HttpOnly  (El cliente no podrá leer la cookie en su browser usando JS!!, pero sí lo
+    //podría hacer modificando la cookie en chrome F12)
+    //  res.setHeader('Set-Cookie','loggedIn=true; Expires=');
+
+
+    //usaremos session para cargar un valor (en este caso boolean) que nos indique 
+    //que el usuario esta logueado. Obviamente para eso se necesita una logica de validacion de 
+    //credenciales, pero asumiremos que las credenciales fueron validas!!
+    //express session nos guarda la session en una variable anexada al request en el momento
+    //que el MW de app.js creo la sesion!!
+    req.session.loggedIn = true;//asignamos una variable nuestra para que indique que el user está logueado
+
+
 
     res.redirect('/');
 };
