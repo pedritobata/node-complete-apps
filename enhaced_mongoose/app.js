2ea1813b7c86a7b4fae2b909fc4123d53418cfda
Pedro Martinez <pedromartinez@MacBook-Pro-de-Pedro.local>
2019-11-12 13:55:28 -0500

    protecting routes and views using csurf

--- enhaced_mongoose/app.js
+++ enhaced_mongoose/app.js
@@ -11,6 +11,9 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const MONGODB_URI = 'mongodb+srv://pedro:R@tamacue1@cluster0-rw1t7.mongodb.net/shop?';
 
+const csrf = require('csurf');
+const csrfProtection = csrf();//tambien podriamos mandar un argumento a csrf para confifurar cosas
+
 const app = express();
 
 const store = new MongoDBStore({
@@ -40,6 +43,15 @@ app.use(session({
   store: store
 }));
 
+//este MW creará tokens para cada post que el usuario dispare.
+//estos tokens deben ser agregados a las vistas para que las identifiquen de posibles ataques
+//de Cross Site Request Fogerty.  mi aplicacion solo responderá a las vistas que tengas su token y no
+//a posibles fake pages que quieran imitar a las mias
+//Luego mas abajo usamos otro MW para que genere este token y se lo agregue al request que invoca a cada
+//vista. La vista cogerá ese token y lo agregará en un input hidden con el nombre exacto "_csrf"
+//Hay que agregar a mano ese campo hidden en todas las vistas  que tengan un form post !!!
+app.use(csrfProtection);
+
 app.use((req, res, next) => {
   if(!req.session.user){
     return next();//pongo return para que ya no se ejecute el resto de este metodo,el next por
@@ -53,6 +65,25 @@ app.use((req, res, next) => {
     })
     .catch(err => console.log(err));
 });
+
+//MW para agregar parametros a todas las peticiones
+//agregamos el token creado por csrf a cada vista 
+//para que luego estas lo envian en las peticiones post y nuestra app las reconozca
+app.use((req,res,next)=>{
+  //locals es una propiedad especial de express que representa la petidcion que
+  //se está enviando hacia el siguiente componente o vista
+  //se comporta igual que el segundo argumento del res.render()
+  //envio el parametro de autenticacion a la vista
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+
+  //envio el token de csrf a la vista gracias al metodo csrfToken() proporcionado por csurf
+  res.locals.csrfToken = req.csrfToken();
+  
+  next();
+});
+
+
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
