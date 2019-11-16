804d7450ed05ec8885f8ef6c5b60b6b683c7d6fe
Pedro Martinez <pedromartinez@MacBook-Pro-de-Pedro.local>
2019-11-16 11:48:53 -0500

    Validation completed

--- enhaced_mongoose/controllers/admin.js
+++ enhaced_mongoose/controllers/admin.js
@@ -1,10 +1,14 @@
 const Product = require('../models/product');
+const { validationResult } = require('express-validator');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
+    hasError: false,
+    errorMessage: null,
+    validationErrors: []
   });
 };
 
@@ -13,6 +17,25 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(422).render('admin/edit-product', {
+              pageTitle: 'Add Product',
+              path: '/admin/add-product',
+              editing: false,
+              hasError: true,
+              product: {
+                  title: title,
+                  price: price,
+                  description: description,
+                  imageUrl: imageUrl
+              },
+              errorMessage: errors.array()[0].msg
+           });
+  };
+
+
   const product = new Product({
     title: title,
     price: price,
@@ -47,7 +70,10 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product
+        product: product,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: []
       });
     })
     .catch(err => console.log(err));
@@ -59,12 +85,31 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(422).render('admin/edit-product', {
+              pageTitle: 'Edit Product',
+              path: '/admin/edit-product',
+              editing: true,
+              hasError: true,
+              product: {
+                  title: updatedTitle,
+                  price: updatedPrice,
+                  description: updatedDesc,
+                  imageUrl: updatedImageUrl,
+                  _id: prodId
+              },
+              errorMessage: errors.array()[0].msg,
+              validationErrors: errors.array()
+           });
+  };
   
   Product.findById(prodId)
     .then(product => {
       //AUTHORIZATION
       //solo voy a editar los productos que haya creado el current user
-      if(product.userId.toSring() !== req.user._id.toString()){
+      if(String(product.userId) !== req.user._id.toString()){
         return res.redirect('/');
       }
       product.title = updatedTitle;
@@ -88,7 +133,6 @@ exports.getProducts = (req, res, next) => {
     // .select('title price -_id')
     // .populate('userId', 'name')
     .then(products => {
-      console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
--- enhaced_mongoose/routes/admin.js
+++ enhaced_mongoose/routes/admin.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
+const { body } = require('express-validator');
 
 const router = express.Router();
 
@@ -15,11 +16,39 @@ router.get('/add-product',isAuth, adminController.getAddProduct);
 router.get('/products',isAuth,  adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product',isAuth,  adminController.postAddProduct);
+router.post('/add-product',[
+    body('title')
+        .isString()
+        .trim()
+        .isLength({ min: 5 }),
+    body('imageUrl')
+        .isURL(),
+    body('price')
+        .isFloat(),
+    body('description')
+        .isString()
+        .trim()
+        .isLength({ min: 5 }),
+
+] ,isAuth,  adminController.postAddProduct);
 
 router.get('/edit-product/:productId',isAuth,  adminController.getEditProduct);
 
-router.post('/edit-product',isAuth,  adminController.postEditProduct);
+router.post('/edit-product',[
+    body('title')
+        .isString()
+        .trim()
+        .isLength({ min: 5 }),
+    body('imageUrl')
+        .isURL(),
+    body('price')
+        .isFloat(),
+    body('description')
+        .isString()
+        .trim()
+        .isLength({ min: 5 }),
+
+] ,isAuth,  adminController.postEditProduct);
 
 router.post('/delete-product',isAuth,  adminController.postDeleteProduct);
 
