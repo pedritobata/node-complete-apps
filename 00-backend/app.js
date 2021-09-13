a183c94391c25de7c3e08e97174c41607c07c5a7
pedro martinez <pedromartinez_goyo@hotmail.com>
2021-09-13 17:55:42 -0500

    project 00: Save post with mongoose

--- 00-backend/app.js
+++ 00-backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const feedRoutes = require('./routes/feed.routes');
@@ -12,4 +13,14 @@ app.use(express.json());
 // Routes
 app.use('/feed', feedRoutes);
 
-app.listen(8080, () => console.log('Server running...'))
+// Mongo DB
+mongoose.connect('mongodb+srv://cluster0.rw1t7.mongodb.net/messages?retryWrites=true&w=majority', {
+      "user": "pedro",
+      "pass": "R@tamacue1",
+      useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(result => {
+    app.listen(8080, () => console.log('Server running...'))
+}).catch(err => console.log('Error connecting to Mongo', err))
+
--- 00-backend/controllers/feed.controller.js
+++ 00-backend/controllers/feed.controller.js
@@ -1,40 +1,44 @@
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require("express-validator/check");
+const PostModel = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-    const posts = [
-        {
-            _id: '1',
-            creator: {
-                name: 'Perico Martinez'
-            },
-            createdAt: new Date(),
-            title: 'First Post',
-            imageUrl: '',
-            content: 'This is my first dummy post. Hope you enjoy it'
-        }
-    ]
-    res.json({posts, totalitems: posts.length});
-}
+  const posts = [
+    {
+      _id: "1",
+      creator: {
+        name: "Perico Martinez",
+      },
+      createdAt: new Date(),
+      title: "First Post",
+      imageUrl: "",
+      content: "This is my first dummy post. Hope you enjoy it",
+    },
+  ];
+  res.json({ posts, totalitems: posts.length });
+};
 
 exports.createPost = (req, res, next) => {
-    console.log('New post', req.body);
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        res.status(422).json({message: 'Invalid post data', errors: errors.array()});
-    }
-    const title = req.body.title;
-    const content = req.body.content;
-
-    res.status(201).json(
-        {
-            post: {
-                _id: '2',
-                title,
-                content,
-                creator: {name: 'Yerbita'},
-                createdAt: new Date()
-            }
-           
-        }
-    )
-}
+  console.log("New post", req.body);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res
+      .status(422)
+      .json({ message: "Invalid post data", errors: errors.array() });
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const newPost = new PostModel({
+    title,
+    content,
+    imageUrl: "some/random/url.jpeg",
+    creator: { name: "Periquito" },
+  });
+  newPost
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        post: result,
+      });
+    })
+    .catch((err) => console.log("Could not save the Post to DB", err));
+};
